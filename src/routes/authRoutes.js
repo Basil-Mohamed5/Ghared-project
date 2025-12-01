5aa575ee2aab3b069408c3d44729b48ef3e469bf
Basil-Mohamed5 <basilmohamedfahmy@gmail.com>
2025-12-01 15:05:57 +0200

    Add files via upload

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,13 @@
-// src/routes/authRoutes.js
 import express from "express";
 import { register, login } from "../controllers/authController.js";
+import asyncWrapper from "../middleware/asyncWrapper.js";
+
 const router = express.Router();
-router.post("/register", register);
-router.post("/login", login);
+
+// Register
+router.post("/register", asyncWrapper(register));
+
+// Login
+router.post("/login", asyncWrapper(login));
+
 export default router;
--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,14 +1,19 @@
 import express from "express";
-import { createTransaction, getTransactionTypes } from "../controllers/transactionController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { createTransaction, getTransactionTypes, getInbox, getSent } from "../controllers/transactionController.js";
 import asyncWrapper from "../middleware/asyncWrapper.js";
 
 const router = express.Router();
 
 // Get Transaction Types
-router.get("/types", protect, asyncWrapper(getTransactionTypes));
+router.get("/types", asyncWrapper(getTransactionTypes));
 
 // Create Transaction
-router.post("/", protect, asyncWrapper(createTransaction));
+router.post("/", asyncWrapper(createTransaction));
+
+// Get Inbox (Received Transactions)
+router.get("/inbox", asyncWrapper(getInbox));
+
+// Get Sent Transactions
+router.get("/sent", asyncWrapper(getSent));
 
 export default router;
